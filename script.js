88c528b866b82fd5b82fdd8bc0a16d95d84738e0
dyshayyf <dyshaferiishta@gmail.com>
2026-04-12 20:57:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,39 +7,3 @@ function cancel() {
     const navbar = document.querySelector(".dropdown");
     navbar.style.transform = "translateY(-500px)";
 }
-
-/* Typewriter */
-const texts = [
-    "stay tuned!",
-    "fresh updates!",
-    "breaking news!"
-];
-
-let speed = 100;
-const textElement = document.querySelector(".typewriter span");
-
-let textIndex = 0;
-let characterIndex = 0;
-
-function typeWriter() {
-    if (characterIndex < texts[textIndex].length) {
-        textElement.innerHTML += texts[textIndex].charAt(characterIndex);
-        characterIndex++;
-        setTimeout(typeWriter, speed);
-    } else {
-        setTimeout(eraseText, 1000);
-    }
-}
-
-function eraseText() {
-    if (textElement.innerHTML.length > 0) {
-        textElement.innerHTML = textElement.innerHTML.slice(0, -1);
-        setTimeout(eraseText, 50);
-    } else {
-        textIndex = (textIndex + 1) % texts.length;
-        characterIndex = 0;
-        setTimeout(typeWriter, 500);
-    }
-}
-
-window.onload = typeWriter;
